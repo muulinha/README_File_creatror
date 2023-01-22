34069a20f923f43fbbbe52ac7e7bda56ec4addc1
muulinha <lucasfl.araujo@hotmail.com>
2023-01-23 06:10:10 +0800

    Fixing issues

---
+++ assets/utills/generateMarkdown.js
@@ -0,0 +1,72 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    if (license === 'None') {
+      return ''
+    }
+    return `![Github badge](https://img.shields.io/badge/license-${license}-blue)`
+  }
+  
+  // TODO: Create a function that returns the license link
+  // If there is no license, return an empty string
+  function renderLicenseLink(license) {
+    if (license === 'None') {
+      return ''
+    }
+    return `* [License](#license)`
+  }
+  
+  // TODO: Create a function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {
+    if (license === 'None') {
+      return ''
+    }
+    return `## License
+  
+    This project is covered under the ${license} license.
+    `
+  }
+  
+  // TODO: Create a function to generate markdown for README
+  function generateMarkdown(data) {
+    return ` # ${data.title}
+    ${renderLicenseBadge(data.license)}
+  
+  ## DESCRIPTION
+  ${data.description}
+  
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  * [Contribution](#contribution)
+  * [Test](#test)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Contribution
+  ${data.contribution}
+  
+  ## Test
+  ${data.test}
+  
+  
+  ## Questions
+  
+  to reach me for any questions contact me at my:
+  
+  Github: [${data.github}](https://github.com/${data.github})
+  E-mail:${data.email}
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
