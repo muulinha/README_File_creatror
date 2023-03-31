d530dbdc7dcfc4c01fad72f4b50bbb59500ea6ab
muulinha <lucasfl.araujo@hotmail.com>
2023-03-31 14:43:28 +0800

    reworded some sentences.

--- assets/utills/generateMarkdown.js
+++ assets/utills/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
     if (license === 'None') {
       return ''
@@ -7,17 +6,14 @@ function renderLicenseBadge(license) {
     return `![Github badge](https://img.shields.io/badge/license-${license}-blue)`
   }
   
-  // TODO: Create a function that returns the license link
-  // If there is no license, return an empty string
+
   function renderLicenseLink(license) {
     if (license === 'None') {
       return ''
     }
     return `* [License](#license)`
   }
-  
-  // TODO: Create a function that returns the license section of README
-  // If there is no license, return an empty string
+
   function renderLicenseSection(license) {
     if (license === 'None') {
       return ''
@@ -28,7 +24,6 @@ function renderLicenseBadge(license) {
     `
   }
   
-  // TODO: Create a function to generate markdown for README
   function generateMarkdown(data) {
     return ` # ${data.title}
     ${renderLicenseBadge(data.license)}
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./assets/utills/generateMarkdown');
 const path = require('path');
 
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type:'input',
         name:'title',
-        message:"what is the title of your project?"
+        message:"what is the project title?"
     },
     {
         type:'input',
@@ -56,18 +54,15 @@ const questions = [
 
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 
 
-// TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((response) => {
         writeToFile('README.md', generateMarkdown(response)) 
      })
 }
 
-// Function call to initialize app
 init();
